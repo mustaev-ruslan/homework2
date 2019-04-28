8201805c18f593be15f578f06d544b0179390223
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2019-04-28 23:23:41 +0300

    Удалил Shell

--- src/main/java/ru/aaxee/homework2/config/ApplicationConfig.java
+++ src/main/java/ru/aaxee/homework2/config/ApplicationConfig.java
@@ -1,15 +1,8 @@
 package ru.aaxee.homework2.config;
 
-import org.jline.utils.AttributedString;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.shell.jline.PromptProvider;
 
 @Configuration
 public class ApplicationConfig {
 
-    @Bean
-    public PromptProvider promptProvider() {
-        return () -> new AttributedString("library:>");
-    }
 }
