abc1988a0fc970e75a53aeb601cb7d0dddb0d54d
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2019-05-02 20:07:36 +0300

    Добавил UserDetailsService

---
+++ src/main/java/ru/aaxee/homework2/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package ru.aaxee.homework2.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JpaUserDetailsService userDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/h2-console").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+        ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(4);
+    }
+}
---
+++ src/main/java/ru/aaxee/homework2/security/User.java
@@ -0,0 +1,20 @@
+package ru.aaxee.homework2.security;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+}
